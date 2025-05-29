df4025b67730932472ac6b56db98a6e82496b7fd
Arthur <Arthur.fabricyo@gmail.com>
2025-05-29 05:59:33 -0300

    Adição das bibliotecas necessárias e melhorias

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,4 +1,6 @@
 import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+import { AntDesign } from "@expo/vector-icons"
+import {CircleCheck} from 'lucide-react-native';
 
 import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
@@ -11,6 +13,7 @@ type Props = TouchableOpacityProps & {
 export function Filter({status, isActive, ...rest}: Props){
     return(
         <TouchableOpacity {...rest} style={[styles.container, {opacity:isActive ? 1 : 0.5 }]}>
+            <CircleCheck size={18}/>
             <Text style={styles.title}>
                 {status===FilterStatus.DONE ? "Comprados" : "Pendentes"}
             </Text>
