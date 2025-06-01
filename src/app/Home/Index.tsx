195c914a87f4502d82ee0da153280365a75c69e9
Arthur <Arthur.fabricyo@gmail.com>
2025-05-31 23:04:19 -0300

    Adição do state para o filtro de status

--- src/app/Home/Index.tsx
+++ src/app/Home/Index.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Image, TouchableOpacity , Text, View } from 'react-native';
+import { Image, TouchableOpacity , Text, View, FlatList } from 'react-native';
 import { styles } from "./styles"
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
@@ -8,15 +9,25 @@ import { FilterStatus } from '@/types/FilterStatus';
 import { Item } from '@/components/Item';
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+const ITEMS = [
+  
+  {id: "1", status: FilterStatus.DONE, description: "Comprar 1 pacote de café"},
+  {id: "2", status: FilterStatus.PENDING, description: "Comprar 3 pacotes de macarrão"},
+  {id: "3", status: FilterStatus.PENDING, description: "Comprar 3 pacotes de cebolas"}
+  
+
+]
 
 export default function Home() {
+    const [filter, setFilter] = useState<FilterStatus>()
+
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} >
         
       </Image>
       <View style={styles.form}>
-      <Input/>
+      <Input placeholder='O que você precisa comprar ?'/>
       <Button title="Entrar"/>
       </View>
 
@@ -24,19 +35,36 @@ export default function Home() {
         <View style={styles.header}>
         {
           FILTER_STATUS.map((status) =>(
-            <Filter key={status} status={FilterStatus.DONE} isActive/>
+            <Filter key={status} 
+            status={FilterStatus.DONE} 
+            isActive ={status ===filter}
+            onPress={()=> setFilter(status)}
+            />
+            
           ))
         }
         <TouchableOpacity style={styles.clearButtom}>
           <Text style={styles.clearText}>Limpar</Text>
         </TouchableOpacity>
         </View>
+    <FlatList
+      data={ITEMS}
+      keyExtractor={(item) => item.id}
+      renderItem={({item}) =>(  
         <Item 
-        data={{status: FilterStatus.DONE, description: "Café"}}
+        data={item}
         onRemove={() =>console.log("Remover item")}
         onStatus={() =>console.log("Alterar Status")}
         />
+      )}
+      showsVerticalScrollIndicator= {false}
+      ItemSeparatorComponent={() => <View style={styles.separator}/>}
+      contentContainerStyle={styles.listContent}
+      ListEmptyComponent={() =>  <Text style={styles.empty}>Nenhum item aqui</Text>}
+      
+    />
       </View>
+      
     </View>
     
   );
