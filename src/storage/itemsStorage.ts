731b08bae093e868bb9e3117c49ea5413b3fb8e6
Arthur <Arthur.fabricyo@gmail.com>
2025-06-04 22:44:49 -0300

    comentários na página de storge

--- src/storage/itemsStorage.ts
+++ src/storage/itemsStorage.ts
@@ -8,7 +8,7 @@ export type ItemStorge = {
     status: FilterStatus,
     description: string
 }
-
+//Faz uma busca de todos os itens dessa tabela
 async function get(): Promise<ItemStorge[]> {
     try {
         const storge = await AsyncStorage.getItem(ITEMS_STORGE_KEY)
@@ -19,10 +19,12 @@ async function get(): Promise<ItemStorge[]> {
         throw new Error("ITEMS_GET: " + error)
     }
 }
+//Faz um filtro de todos os itens com base num parâmetro
 async function getByStatus(status: FilterStatus): Promise<ItemStorge[]> {
     const items = await get()
     return items.filter((item) => item.status === status)
 }
+//Salva os itens dentro do banco de dados do dispositivo
 async function save(items: ItemStorge[]): Promise<void> {
     try {
         await AsyncStorage.setItem(ITEMS_STORGE_KEY, JSON.stringify(items))
@@ -30,6 +32,7 @@ async function save(items: ItemStorge[]): Promise<void> {
         throw new Error("ITEMS_SAVE: " + error)
     }
 }
+//Adiciona um item no banco de dados do dispositivo
 async function add(newItem: ItemStorge): Promise<ItemStorge[]> {
     const items = await get()
     const updatedItems = [...items, newItem]
@@ -38,12 +41,13 @@ async function add(newItem: ItemStorge): Promise<ItemStorge[]> {
     return updatedItems 
 
 }
-
+//Remove um item do banco de dadods do dispositivo
 async function remove(id: string): Promise<void> {
     const items = await get()
     const updatedItems = items.filter((item) => item.id !== id)
     save(updatedItems)
 }
+//Limpa os itens do banco de dados do dispositivo
 async function clear(): Promise<void> {
     try {
         await AsyncStorage.removeItem(ITEMS_STORGE_KEY)
@@ -51,6 +55,7 @@ async function clear(): Promise<void> {
         throw new Error("ITEMS_CLEAR: " + error)
     }
 }
+//Altera o status do item 
 async function toggleStatus(id: string): Promise<ItemStorge[]> {
     const items = await get()
 
--- src/types/FilterStatus.ts
+++ src/types/FilterStatus.ts
@@ -2,3 +2,5 @@ export enum FilterStatus{
     PENDING = "Pending",
     DONE = "Done"
 }
+
+//Tipagem dos tipos de filtros
